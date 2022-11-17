3050418cd2694fc42c92689e0e513a3c4a50e757
Android-JCPP-Servo <astewart1138@gmail.com>
2022-11-17 12:39:56 -0600

    Add Red Part 1B

--- tdd_factorial/test/index_test.js
+++ tdd_factorial/test/index_test.js
@@ -4,7 +4,13 @@ var Calculate =  require('../index.js')
 describe('Calculate', () => {
     describe('.factorial', () => {
         it('test if the output of 5! is 120', () => {
-        
+            // Setup
+            const baseNumber = 5;
+            const expected = 120;
+            // Exercise
+            const result = Calculate.factorial(baseNumber);
+            // Verification
+            assert.strictEqual(result, expected);        
         })
     });
 });
