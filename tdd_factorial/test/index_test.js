93cdaa4c8b2aa57ed72cedb9fcd109c3fee7a239
Android-JCPP-Servo <astewart1138@gmail.com>
2022-11-17 12:36:48 -0600

    Add index test js file

---
+++ tdd_factorial/test/index_test.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+var Calculate =  require('../index.js')
+
+describe('Calculate', () => {
+    describe('.factorial', () => {
+        it('test if the output of 5! is 120', () => {
+        
+        })
+    });
+});
